1b1b6cc540080ffc021a4cd30f1987a5842a373b
Karthik <karthik.jk11@gmail.com>
2018-09-08 00:06:31 +0530

    Modify Package.json

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public/dist/public')));
-app.use('/', express.static(path.join(__dirname, 'public/dist/public')));
+app.use(express.static(path.join(__dirname, 'dist/public')));
+app.use('/', express.static(path.join(__dirname, 'dist/public')));
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
