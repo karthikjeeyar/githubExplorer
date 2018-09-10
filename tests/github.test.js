1b131f42ad236a52b27f58a3432f793441291939
Karthik <karthik.jk11@gmail.com>
2018-09-10 15:30:51 +0530

    Add Test doubles and test cases

---
+++ tests/github.test.js
@@ -0,0 +1,29 @@
+const mockAxios = require('./__mocks__/axios');
+const github = require("../models/github");
+const userMockData = require('./__mockData__/userDetail.json');
+const usersMockData = require('./__mockData__/usersDetail.json');
+
+it("fetches user information from github", async () => {
+    mockAxios.get.mockImplementationOnce(() =>
+        Promise.resolve({
+            data: userMockData
+
+        })
+    );
+    const user = await github.getUserInfo("karthikjeeyar");
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(user).toBeDefined()
+    expect(user.login).toEqual('karthikjeeyar')
+});
+
+it("fetches list of users from github", async () => {
+  mockAxios.get.mockImplementation(() =>
+    Promise.resolve({
+          data: usersMockData
+    })
+  );
+  const users = await github.getUsersList("karthikj");
+  expect(users).toBeDefined();
+  expect(users.total_count).toEqual(2);
+});
+
