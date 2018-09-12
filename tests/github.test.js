7540432ebbdb5a8d9f740eb78ffb0b64f891721d
Karthik <karthik.jk11@gmail.com>
2018-09-12 09:49:15 +0530

    Add watcher mode for test cases

--- tests/github.test.js
+++ tests/github.test.js
@@ -16,14 +16,19 @@ it("fetches user information from github", async () => {
     expect(user.login).toEqual('karthikjeeyar')
 });
 
-it("fetches list of users from github", async () => {
-  mockAxios.get.mockImplementation(() =>
-    Promise.resolve({
-          data: usersMockData
-    })
-  );
-  const users = await github.getUsersList("karthikj");
-  expect(users).toBeDefined();
-  expect(users.total_count).toEqual(2);
+it("should return empty Object in absence of mock implementaion", async () => {
+    const users = await github.getUsersList("karthikj");
+    expect(users).toBeDefined();
+    expect(users).toEqual({});
 });
 
+it("fetches list of users from github", async () => {
+    mockAxios.get.mockImplementation(() =>
+        Promise.resolve({
+            data: usersMockData
+        })
+    );
+    const users = await github.getUsersList("karthikj");
+    expect(users).toBeDefined();
+    expect(users.total_count).toEqual(2);
+});
