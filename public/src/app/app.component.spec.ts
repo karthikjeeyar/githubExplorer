84d80e25086b1bceb0785d805a689adf3b2f3a51
Karthik <karthik.jk11@gmail.com>
2018-09-13 21:20:23 +0530

    Add Search user test

--- public/src/app/app.component.spec.ts
+++ public/src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async, tick, fakeAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from "@angular/http";
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let debugElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -15,16 +20,19 @@ describe('AppComponent', () => {
         FormsModule,
         HttpModule
       ]
-    }).compileComponents();
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+      });
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'GitExplorer'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     console.log('app', app)
     expect(app.searchString).toEqual("");
@@ -32,9 +40,19 @@ describe('AppComponent', () => {
   }));
 
   it('should render a valid title', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.logo__title').textContent).toContain('Git Explorer');
+  }));
+
+  it(`should call the searchUser method'`, async(async() => {
+    const compiled = fixture.debugElement.nativeElement;
+
+    spyOn(component, "searchUser");
+    component.searchString = "karthikjeeyar";
+    compiled.querySelector(".search__button").click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(component.searchUser).toHaveBeenCalled();
   }));
 });
